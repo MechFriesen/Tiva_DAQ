5f7549e360536d9e39c200038dbde57c1b405c5b
MechFriesen <32364356+MechFriesen@users.noreply.github.com>
2017-11-21 16:06:58 -0500

    fuck this. I guess I'll commit... if I have to

---
+++ Frost_Probe/Debug/Main.d
@@ -0,0 +1,61 @@
+# FIXED
+
+Main.obj: ../Main.c
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdio.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/adc.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/debug.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/pin_map.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/uart.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/utils/uartstdio.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/time.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/abi_prefix.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/fpu.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/hibernate.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/systick.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/rom.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/utils/ustdlib.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h
+Main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/time.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/inc/hw_hibernate.h
+Main.obj: ../datetimeset.h
+Main.obj: C:/ti/TivaWare_C_Series-2.1.3.156/grlib/grlib.h
+Main.obj: ../FP_acquire.h
+
+../Main.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/string.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/utils/uartstdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/time.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/abi_prefix.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/hibernate.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/utils/ustdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/time.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/inc/hw_hibernate.h: 
+../datetimeset.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/grlib/grlib.h: 
+../FP_acquire.h: 
